ee170f51851ebcc567df4cec6fe380bac366b366
limjihoon <lonelynight1026@hamagroups.io>
2025-04-03 17:29:41 +0900

    Initial commit

---
+++ src/main/java/com/example/embeddedredislockpoc/EmbeddedRedisLockPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.embeddedredislockpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedRedisLockPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedRedisLockPocApplication.class, args);
+    }
+
+}
